fce03bd7b77ee21f0272c70c0ae6eeca69f321a1
phatdroid94 <phatdroid94@gmail.com>
2019-09-25 18:43:14 +0700

    init

---
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/youtubeapiv329072019/MainActivity.java
@@ -0,0 +1,15 @@
+package phamtanphat.ptp.khoaphamtraining.youtubeapiv329072019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    String API_KEY = "AIzaSyAhKNr7hCcAkYiwSeWNmHePoxHjvV8H3Uc";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
