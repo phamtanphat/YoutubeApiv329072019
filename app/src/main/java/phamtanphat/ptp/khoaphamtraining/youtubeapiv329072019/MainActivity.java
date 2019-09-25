a2957010402a6cc423562523b583020a00838daf
phatdroid94 <phatdroid94@gmail.com>
2019-09-25 19:00:28 +0700

    a

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/youtubeapiv329072019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/youtubeapiv329072019/MainActivity.java
@@ -1,17 +1,45 @@
 package phamtanphat.ptp.khoaphamtraining.youtubeapiv329072019;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends AppCompatActivity implements YouTubePlayer.OnInitializedListener {
 
 
+    YouTubePlayerView youTubePlayerView;
     String API_KEY = "AIzaSyAhKNr7hCcAkYiwSeWNmHePoxHjvV8H3Uc";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        youTubePlayerView = findViewById(R.id.youtubePlayerview);
+
+        youTubePlayerView.initialize(API_KEY,this);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        youTubePlayer.loadVideo("d9xm3kXffGo");
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        if (youTubeInitializationResult.isUserRecoverableError()){
+            youTubeInitializationResult.getErrorDialog(MainActivity.this,123);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
 
