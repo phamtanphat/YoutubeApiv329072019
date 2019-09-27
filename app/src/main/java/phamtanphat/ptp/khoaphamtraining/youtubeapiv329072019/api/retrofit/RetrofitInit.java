d55660952a2cb4702682c7ffefedd2f0a0047834
phatdroid94 <phatdroid94@gmail.com>
2019-09-27 19:05:32 +0700

    a

---
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/youtubeapiv329072019/api/retrofit/RetrofitInit.java
@@ -0,0 +1,44 @@
+package phamtanphat.ptp.khoaphamtraining.youtubeapiv329072019.api.retrofit;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.sql.Time;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+// Tao ra 1 singleTon tranh viec khoi tao qua nhiu retrofit
+public class RetrofitInit {
+    private static Retrofit mretrofit = null;
+    private RetrofitInit(){
+
+    }
+    public static Retrofit getInstance(String base_url){
+        if (mretrofit == null){
+
+            Gson gson  = new GsonBuilder().setLenient().create();
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                                .readTimeout(10 , TimeUnit.SECONDS)
+                                .writeTimeout(10 , TimeUnit.SECONDS)
+                                .connectTimeout(10,TimeUnit.SECONDS)
+                                .retryOnConnectionFailure(true)
+                                .protocols(Arrays.asList(Protocol.HTTP_1_1))
+                                .build();
+
+
+            mretrofit = new Retrofit.Builder()
+                            .client(okHttpClient)
+                            .addConverterFactory(GsonConverterFactory.create(gson))
+                            .baseUrl(base_url)
+                            .build();
+
+
+        }
+        return  mretrofit;
+    }
+}
