8334b60e509daa17b8e2dc2265957602e548830b
phatdroid94 <phatdroid94@gmail.com>
2019-09-30 18:47:00 +0700

    a

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/youtubeapiv329072019/api/modelapi/High.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/youtubeapiv329072019/api/modelapi/High.java
@@ -5,38 +5,16 @@ import com.google.gson.annotations.SerializedName;
 
 public class High {
 
-@SerializedName("url")
-@Expose
-private String url;
-@SerializedName("width")
-@Expose
-private Integer width;
-@SerializedName("height")
-@Expose
-private Integer height;
+    @SerializedName("url")
+    @Expose
+    private String url;
 
-public String getUrl() {
-return url;
-}
+    public String getUrl() {
+        return url;
+    }
 
-public void setUrl(String url) {
-this.url = url;
-}
-
-public Integer getWidth() {
-return width;
-}
-
-public void setWidth(Integer width) {
-this.width = width;
-}
-
-public Integer getHeight() {
-return height;
-}
-
-public void setHeight(Integer height) {
-this.height = height;
-}
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
 }
